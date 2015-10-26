9d936d2579a28810b9bca33676fe61bb1aef0b7f
Ian McDonald <iamcdonald@c02gm6kqdv7p.rim.net>
2015-10-26 07:14:57 -0400

    test reporter changes.

--- test_reporter/TestReporter.py
+++ test_reporter/TestReporter.py
@@ -9,14 +9,40 @@ from my_log import *
 from my_sql import *
 
 class TestReporter(My_SQL):
-	def junk(self):
-		pass
 
-dog = TestReporter("serenity.bts.rim.net", "root", "q1!w2@e3#");
+	def create_project_table(self, ine="IF NOT EXISTS"):
+		query = "CREATE TABLE " + ine + " `project` ("\
+				"`id` int(11) NOT NULL AUTO_INCREMENT,"\
+ 				"`name` varchar(60) NOT NULL,"\
+				"`comment` text,"\
+				"`created` datetime NOT NULL,"\
+				"PRIMARY KEY (`id`),"\
+				"UNIQUE KEY `name_UNIQUE` (`name`)"\
+				") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+		return self.query(query)
+
+	def setup_database(self, db_name=None):
+		data_bases = self.list_databases()
+
+		if data_bases is not None:
+			rc = True
+
+			if db_name not in data_bases:
+				rc = self.create_db(db_name)
+
+			if rc:
+				rc = self.select_db(db_name)
+
+			if rc:
+				rc = self.create_project_table();
+
+
+			return rc
+		else:
+			return False
+
+
+dog = TestReporter("serenity.bts.rim.net", <user_name>, <password>);
 
 dog.connect()
-print dog.list_tables("qa_db")
-
-#dog.create_db("fred")
-#dog.select_db("fred")
-#dog.query(" CREATE TABLE MyGuests (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,firstname VARCHAR(30) NOT NULL,lastname VARCHAR(30) NOT NULL, email VARCHAR(50),reg_date TIMESTAMP)");
+print dog.setup_database("fred")
