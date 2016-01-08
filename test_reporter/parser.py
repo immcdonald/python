e7d3a49d5167f7f039d44c048e27c4517270785e
Ian McDonald <dlanodcm@gmail.com>
2016-01-08 16:00:31 -0500

    worked on some file log improvements.. and started some yoyo.sum parsing

--- test_reporter/parser.py
+++ test_reporter/parser.py
@@ -1,10 +1,78 @@
 import argparse
-import sys
+import sys, os, shutil
+import re
 import user
-from TestReporter import TestReporter
+from TestReporter import TestReporter, DEBUG, ERROR
+
 from pprint import pformat
 
-def add_project(args):
+
+test_point_prefix = ["METRIC",
+					 "START",
+					 "STOP",
+					 "POINT",
+					 "PASSX",
+					 "PASS",
+					 "FAILX",
+					 "FAIL",
+					 "UNRES",
+					 "UNSUP",
+					 "BOOT",
+					 "boot",
+					 "WARN",
+					 "ERR",
+					 "NOTE",
+					 "ABORT"]
+
+arch_type_file_path_breaks = ["$PROCTYPE",
+							  "aarch64",
+							  "arm",
+							  "x86",
+							  "x86_64",
+							  "ssh",
+							  "mips",
+							  "ppc"
+							]
+
+
+
+def generate_regex_prefix():
+	start = []
+	middle = []
+	end = []
+
+	max_middle_size = 0
+	min_middle_size = 10000000000
+
+	for prefix in test_point_prefix:
+		size = len(prefix)
+
+		size = size - 2;
+
+		if (size - 2) > max_middle_size:
+			max_middle_size = size
+
+		if size < min_middle_size:
+			min_middle_size = size
+
+		if prefix[0] not in start:
+			start.append(prefix[0])
+
+		if prefix[-1] not in end:
+			end.append(prefix[-1])
+
+		# Reminder range works from param_0 to param_1 - 1
+		# We should subtract a second 1 because this is used
+		# as a conversion of a length to a list index (starting at 0)
+		# but above we already removed 2 so instead add 1
+		for index in range(1, size + 1):
+			if prefix[index] not in middle:
+				middle.append(prefix[index])
+
+	return "[" + "|".join(sorted(start)) + "][" + "|".join(sorted(middle)) + "]{" + str(min_middle_size) + "," + str(max_middle_size) + "}[" + "|".join(sorted(end)) + "]:\s"
+
+
+def add_project(args, log):
 	arches = ["aarch64", "arm", "mips", "ppc", "sh", "x86", "x86_64"]
 
 	targets = ["adsom-7222",
@@ -178,23 +246,302 @@ def add_project(args):
 
 					return True
 				else:
-					print "Failed to connect to the mysql database."
+					log.out("Failed to connect to the mysql database.", ERROR)
 					return False
 			else:
-				print "Child name is to short"
+				log.out("Child name is to short", ERROR)
 				return False
 		else:
-			print "Project name is to short."
+			log.out("Project name is to short.", ERROR)
 			return False
 	else:
 		return False
 
+def fix_test_point_over_writes(args, log, src_path):
+	regex_test_point_prefix = generate_regex_prefix()
+
+	# regular expresssion to look for any test point prefix that
+	# does't start the line.
+	regex_pattern = "(?P<testpnt>" + generate_regex_prefix() + ")(?P<the_rest>.*\n)"
+
+	tstpnt_regex = re.compile(regex_pattern)
+
+	if os.path.exists(src_path):
+		file_data = []
+		changed = False
+		with open(src_path, "Ur") as fp_in:
+			# use this one so the lines have "\n" at the end.
+			file_data = fp_in.readlines()
+
+		index = 0
+
+		num_of_lines = len(file_data)
+
+		while index < num_of_lines:
+			result = tstpnt_regex.search(file_data[index])
+
+			if result:
+				matches = result.groupdict()
+
+				if "testpnt" in matches:
+
+					# Check to see if the prefix we foudn (minus ': ')
+					# is in the list of known test point prefixes
+					if matches["testpnt"][0:-2] in test_point_prefix:
+
+						tst_pnt_str = matches["testpnt"]
+
+						if "the_rest" in matches:
+							tst_pnt_str = tst_pnt_str + matches["the_rest"]
+
+						if file_data[index].find(tst_pnt_str) > 0:
+							# Calculate the size of the string + 1 to account for the \n
+							size = len(tst_pnt_str) + 1
+
+							# Now strip off the test point line from the end of the string
+							if (index + 1) < num_of_lines:
+								# Add add the end from the next line
+								file_data[index] = file_data[index][0: ((size-1) * -1)] + file_data[index+1]
+							else:
+								# Just strip off the test point part.
+								file_data[index] = file_data[index][0: ((size-1) * -1)]
+
+							# Add the found test point line to the next index.
+							file_data[index+1] = tst_pnt_str
+							changed = True
+							index = index + 1
+			index = index + 1
+
+		if changed and args["no_mod"] == False:
+			with open(src_path, "w") as fp_out:
+				for line in file_data:
+					fp_out.write(line)
+		return True
+	else:
+		return False
+
+
+def scan_for_repeated_lines(args, log, src_path):
+	if os.path.exists(src_path):
+		file_data = []
+		del_lines = []
+
+		changed = False
+		with open(src_path, "Ur") as fp_in:
+			file_data = fp_in.readlines()
+
+		# Add one extra line so we know that if there are dups
+		# to the end of the file they get processed.
+		file_data.append("\n")
+
+		index = 0
+
+		while index < len(file_data):
+			next_line = index + 1
+
+			if len(file_data[index]) == 0 or file_data[index]=="\n":
+				index = index + 1
+				continue
+
+			empty_line_counter = 0
+			while next_line < len(file_data):
+				if len(file_data[next_line]) == 0 or file_data[next_line]=="\n":
+					next_line = next_line + 1
+					empty_line_counter = empty_line_counter + 1
+					continue
+
+				if file_data[index] != file_data[next_line]:
+					dup_line_count = (next_line - index)
+					if dup_line_count > 2:
+						changed = True
+						file_data[index+1] = "Post Parsing Edit: Previous line repeated " +  str(dup_line_count - 1 - empty_line_counter) + " additional times. \n"
+
+						for del_index in range(index+2, next_line):
+							del_lines.append(del_index)
+						index = next_line - 1
+					break
+				next_line = next_line + 1
+			index = index + 1
+
+		# If the file content changed then rewrite the file.
+		if changed and args["no_mod"] == False:
+			for del_index in reversed(del_lines):
+				del file_data[del_index]
+
+			# Remove the extra line we added above.
+			del file_data[-1]
+
+			with open(src_path, "w") as fp_out:
+				for line in file_data:
+					fp_out.write(line)
+
+		return True
+	else:
+		log.out(src_path + " does not exist", ERROR)
+		return False
+
+def cleanup_yoyo_sum(args, log, fp, path):
+	if scan_for_repeated_lines(args, log, os.path.join(path,"yoyo.sum")):
+		return True
+	else:
+		return False
+
+def cleanup_yoyo_log(args, log, fp, path):
+	if scan_for_repeated_lines(args, log, os.path.join(path,"yoyo.log")):
+		if fix_test_point_over_writes(args, log, os.path.join(path,"yoyo.log")):
+			return True
+		else:
+			return False
+	else:
+		return False
+
+def cleanup_log_files(args, log, fp, recovery_data):
+	for root, dirs, files in os.walk(args["input"]):
+		if "yoyo.sum" in files:
+			completed_string = root + " " + "yoyo.sum cleanup completed"
+
+			if completed_string not in recovery_data:
+				if cleanup_yoyo_sum(args, log, fp, root):
+					fp.write(completed_string+"\n")
+				else:
+					return False
+
+		if "yoyo.log" in files:
+			completed_string = root + " " + "yoyo.log cleanup completed"
+
+			if completed_string not in recovery_data:
+				if cleanup_yoyo_log(args, log, fp, root):
+					fp.write(completed_string+"\n")
+				else:
+					return False
+	return True
+
+
+def process_yoyo_sum(args, log, yoyo_sum_path):
+	data = {}
+
+	# regular expresssion to look for any test point prefix that
+	# does't start the line.
+	regex_pattern = "(?P<testpnt>" + generate_regex_prefix() + ")(?P<the_rest>.*)"
+
+	print regex_pattern
+
+
+	tstpnt_regex = re.compile(regex_pattern)
+	with open(os.path.join(yoyo_sum_path, "yoyo.sum"), "Ur") as fp_in:
+		# Read in striping new lines this time.
+		sum_file_data = fp_in.read().splitlines()
+
+		for line in sum_file_data:
+			print line
+			if len(line) == 0:
+				continue
+
+			result = tstpnt_regex.search(line)
+
+			if result:
+				matches = result.groupdict()
+
+				if "testpnt" in matches:
+					print "->", matches["testpnt"], "-->", matches["the_rest"]
+	return data
+
+
+def process_variants(args, log, fp, recovery_data):
+	data = {}
+
+	input_lenght = len(args["input"])
+
+	for root, dirs, files in os.walk(args["input"]):
+
+		relative_root = root[input_lenght:]
+
+		if len(relative_root) > 0:
+			if relative_root[0] == "/":
+				relative_root = relative_root[1:]
+
+		if "yoyo.sum" in files:
+			completed_string = root + " " + "yoyo.sum import completed"
+			if completed_string not in recovery_data:
+				data[relative_root] = process_yoyo_sum(args, log, root)
+
+				if len(data) > 0:
+
+					pass
+
+				else:
+					return False
+
+		if "yoyo.log" in files:
+			completed_string = root + " " + "yoyo.log import completed"
+			if completed_string not in recovery_data:
+				pass
+
+	if args["project"] is None:
+		log.out("No project was specified.", ERROR)
+		return False
+
+	if args["child"] is None:
+		log.out("No child project was specified.", ERROR)
+		return False
+
+	return True
+
+def parse(args, log):
+	if os.path.exists(str(args["input"])):
+		with (open("recovery.txt", "a+")) as fp:
+			fp.seek(0, os.SEEK_SET)
+			# read in the file and split on newline. (this removes the \n caracter from the end of the line.)
+			recovery_data = fp.read().splitlines()
+
+			# Check to see if the recovery file contains a input path
+			# Then check to see if is the same as the current one.
+			if len(recovery_data) > 0:
+				if recovery_data[0] != args["input"]:
+					# If they are not the same then reset this recover file for the new data.
+					recovery_data = []
+					fp.seek(0, os.SEEK_SET)
+					fp.write(str(args["input"]) + "\n")
+			else:
+				fp.write(str(args["input"]) + "\n")
+
+			if cleanup_log_files(args, log, fp,  recovery_data):
+
+				if process_variants(args, log, fp, recovery_data):
+					# Do not return from here, so that the recovery
+					# file gets deleted if we are still on a success path.
+					pass
+				else:
+					return False
+
+		# If we get here and there are no errors. Then delete the recovery file.
+		os.remove("recovery.txt")
+
+	else:
+		log.out("(" + args["input"] + ") does not exist or is not accessible", ERROR)
+		return False
+
+	return True
+
 def main(argv=None):
 	parser = argparse.ArgumentParser(description='Qnx Dejagnu parser')
 
-
 	parser.add_argument('-add_project', '--add_project',
 						action='store_true',
+						help='')
+
+	parser.add_argument('-no_mod', '--no_mod',
+						action='store_true',
+						help='')
+
+
+	parser.add_argument('-project', '--project',
+						help='')
+
+	parser.add_argument('-child', '--child',
+						help='')
+
+	parser.add_argument('-input', '--input',
 						help='')
 
 	parser.add_argument('-set_storage_path', '--set_storage_path',
@@ -208,9 +555,6 @@ def main(argv=None):
 	parser.add_argument('-exec_id', '--exec_id',
 						help='',
 						default=-1)
-
-	parser.add_argument('-input_dir', '--input_dir',
-						help='')
 
 	parser.add_argument('-mysql_host', '--mysql_host',
 						default=user.sql_host,
@@ -257,16 +601,35 @@ def main(argv=None):
 		argv = sys.argv[1:]
 
 	args = vars(parser.parse_args())
+
+
 	print pformat(args)
+
 
 	args["reporter"] = TestReporter(args["mysql_host"],  args["mysql_user_name"], args["mysql_password"], args["schema"])
 
+	log = args["reporter"].get_log()
+
+	log.set_verbosity(DEBUG, args["verbosity"])
+
 	if args["reporter"]:
-		if "add_project" in args:
-			print add_project(args)
+		if args["add_project"]:
+			if add_project(args, log):
+				return 0
+			else:
+				return -1
+
+		elif args["input"]:
+			if parse(args, log):
+				return 0
+			else:
+				return -1
+		else:
+			log.out("No activity command line argument found.", ERROR)
+			return -1
 
 	else:
-		print "Creatation of reporter class failed."
+		log.out("Creatation of reporter class failed.", ERROR)
 		return -1
 
 if __name__ == "__main__":
