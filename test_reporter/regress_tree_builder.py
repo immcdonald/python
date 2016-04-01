3aae08a29cf74e6adcfbd04e3e8d7956c808a788
Ian McDonald <dlanodcm@gmail.com>
2016-04-01 11:50:27 -0400

    Unpacking of archive files complete.. now pulling any missing files from the database..

--- test_reporter/regress_tree_builder.py
+++ test_reporter/regress_tree_builder.py
@@ -5,6 +5,9 @@ from pprint import pformat
 import user
 import json
 import glob
+import subprocess
+import shutil
+
 
 parentPath = os.path.abspath("../common")
 
@@ -41,25 +44,40 @@ def build_tree(args):
 
 		for exec_id in exec_ids:
 			found_regress_data = False
-
 			exec_path = os.path.join(project_path, "%0.9d" % exec_id[0])
-			partial_regress_data_path = os.path.join(args["source"], "new_db_%d_*.tar.gz" % exec_id[0])
-
-
-			glob_match = glob.glob(partial_regress_data_path)
-
-
-
-			if len(glob_match) == 0:
-				found_regress_data = False
-			elif len(glob_match) == 1:
-				print "Put unpacking code here!!!"
-			else:
-				raise Exception("Error: regression data archive search on (" + str(partial_regress_data_path) +  ") return: %s" % pformat(glob_match))
 
 			if os.path.exists(exec_path) == False:
 				# Create the project directory
 				os.makedirs(exec_path)
+
+			partial_regress_data_path = os.path.join(args["source"], "new_db_%d_*.tar.gz" % exec_id[0])
+
+			glob_match = glob.glob(partial_regress_data_path)
+
+			if len(glob_match) == 0:
+				found_regress_data = False
+			elif len(glob_match) == 1:
+				found_regress_data  = True
+				cmd = ["tar", "xf", glob_match[0], "--strip=1", "-C", os.path.abspath(exec_path)]
+				handle = subprocess.Popen(cmd);
+				handle.wait()
+
+				yoyo_path = os.path.join(exec_path,"yoyo.suite/build")
+				if os.path.exists(yoyo_path):
+					for root, dirs, files in os.walk(yoyo_path):
+
+						if len(files) > 0:
+							relative_path =  os.path.relpath(root, yoyo_path)
+
+							test_path = os.path.join(exec_path, relative_path)
+							if os.path.exists(test_path):
+								for file_name in files:
+									shutil.move(os.path.join(root, file_name), os.path.join(test_path, file_name))
+
+					shutil.rmtree(os.path.join(exec_path,"yoyo.suite"))
+
+			else:
+				raise Exception("Error: regression data archive search on (" + str(partial_regress_data_path) +  ") return: %s" % pformat(glob_match))
 
 			table = "sources"
 			fields = ["exec_id"]
